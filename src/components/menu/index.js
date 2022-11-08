41ce082bd393c2bf92428ad5683d2fe45b971880
Martin Christov <martin.christov@gmail.com>
2022-11-08 14:32:17 +0300

    top nav

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { Layout } from 'antd';
 import logo from 'images/gpml.svg';
+import './style.scss'
 
 const MenuBar = () => {
 	return (
@@ -8,8 +9,14 @@ const MenuBar = () => {
 			<Layout.Header className='nav-header-container'>
 				<div className='ui container'>
 					<Link to='/' className='logo-a'>
-						<img src={logo} className='logo' alt='GPML' />
+						<img src="/unep.svg" className="unep" alt="UNEP" />
+            <img src="/cobsea.svg" alt="COBSEA" />
 					</Link>
+          <nav>
+            <Link to='/knowledge-library'>Knowledge library</Link>
+            &nbsp;|&nbsp;
+            <Link to="/research/database">Research database</Link>
+          </nav>
 				</div>
 			</Layout.Header>
 		</>
