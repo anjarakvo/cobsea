ee784ec254ee7f2d017bb06234e74fd4950d7b2d
Martin Christov <martin.christov@gmail.com>
2022-11-15 16:31:34 +0300

    overlay menu wip

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
 import { Layout } from 'antd';
 import classNames from 'classnames'
+import { Fade as Hamburger } from 'hamburger-react'
 import './style.scss'
+import { useState } from 'react';
+import FullMenu from 'components/full-menu/full-menu';
 
 const MenuBar = ({ landing }) => {
+  const [isOpen, setOpen] = useState(false)
 	return (
 		<>
 			<header className={classNames('nav-header', { 'at-landing': landing })}>
@@ -12,13 +16,20 @@ const MenuBar = ({ landing }) => {
 						<img src="/unep.svg" className="unep" alt="UNEP" />
             <img src="/cobsea.svg" alt="COBSEA" />
 					</Link>
-          <nav>
+          {/* <nav>
             <Link to='/knowledge-library'>Knowledge library</Link>
             &nbsp;|&nbsp;
             <Link to="/research">Research database</Link>
-          </nav>
+          </nav> */}
+          <Hamburger size={40} toggled={isOpen} toggle={setOpen} />
 				</div>
 			</header>
+      {!landing && <div className="spacer" />}
+      {isOpen &&
+      <div className="full-menu-overlay">
+        <FullMenu />
+      </div>
+      }
 		</>
 	);
 };
