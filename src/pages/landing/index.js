eff925b1f4e763832c8e1d6f8676c252fde457ee
Martin Christov <martin.christov@gmail.com>
2022-11-15 13:26:19 +0300

    hero styles

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,3 +1,4 @@
+import { Button } from 'antd';
 import FullMenu from 'components/full-menu/full-menu';
 import MenuBar from 'components/menu';
 import React from 'react';
@@ -14,6 +15,7 @@ const Home = () => {
             <span>of the</span><img src="/gpml.svg" alt="GPML" />
           </div>
           <h4>The web platform of the Regional Node provides stakeholders in the East Asian Seas region with access to knowledge, resources and networking services on marine litter and plastic pollution for informed action.</h4>
+          <Button type="ghost" size="large">{`Read more`}</Button>
         </div>
       </div>
       <div className="menu-section">
