b50aa5e105c8697a01d834fbea8c7d34de3cde1d
Navin <nikshingote@gmail.com>
2022-11-18 16:55:40 +0530

    add env js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,16 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  app.use(
-    ["/api", "/image", "/env.js", "/cv"],
-    createProxyMiddleware({
-      target: "https://unep-gpml.akvotest.org/",
-      changeOrigin: true,
-    })
-  );
+	app.use(
+		['/api', '/image', '/env.js', '/cv'],
+		createProxyMiddleware({
+			target: process.env.REACT_APP_FEENV
+				? 'https://unep-gpml.akvotest.org/'
+				: 'http://backend:3000',
+			changeOrigin: true,
+			secure: false,
+			changeOrigin: true,
+			cookieDomainRewrite: '',
+		}),
+	);
 };
