fbdb558ca1dd7f20180b496bc88bca4965acbb4e
Navin <nikshingote@gmail.com>
2022-11-25 15:04:55 +0530

    add svgs in capacity building

--- src/pages/capacity-building/index.js
+++ src/pages/capacity-building/index.js
@@ -16,31 +16,6 @@ const popularTags = [
 	'source to sea',
 ];
 
-const resourceList = [
-	{
-		title: 'Workshops / Trainings (in-person)',
-		ids: [
-			10074, 10065, 10063, 10064, 10060, 10157, 10155, 10154, 10128, 10127,
-			10126, 30, 10043, 22, 10103, 10081, 10082, 33, 36, 28, 10056, 10022, 19,
-			18, 10071, 4,
-		],
-	},
-	{
-		title: 'Webinars & Recordings (online events)',
-		ids: [
-			10156, 10105, 10039, 10026, 10059, 9, 10067, 10047, 10028, 10100, 3,
-			10122, 10114, 10116, 10113, 10110, 10109, 10104, 10080, 10083, 10077,
-			10079, 10041, 10049, 10040, 10036, 10025, 23, 10014, 10012, 10010, 10011,
-			10007, 10006, 10058, 10093, 10094, 10085, 10097, 10057, 10034, 10091,
-			10076, 10090, 8, 31, 10, 10095, 10096,
-		],
-	},
-	{
-		title: 'E-Learning Courses',
-		ids: [10031, 10668, 10197, 149],
-	},
-];
-
 function CapacityBuilding({ setLoginVisible, isAuthenticated }) {
 	const history = useHistory();
 	const box = document.getElementsByClassName('knowledge-lib');
